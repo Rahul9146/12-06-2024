c6ce6873f8040a91729e4e08f9181c8679d84017
Rahul Kaunsalye <141475915+Rahul9146@users.noreply.github.com>
2024-06-17 09:30:01 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,17 @@
+package com.javabasic;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		//Typecasting Example
+		    double dNum = 100.04;
+	        int num = (int) dNum; // double is explicitly cast to int
+
+	        System.out.println("Double value: " + dNum);
+	        System.out.println("Integer value (after narrowing): " + num);
+	    }
+	}
+
+	
+
+
