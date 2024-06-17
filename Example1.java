7fc28cba452f029d3cff080a97e93c8b1301b3dc
Rahul Kaunsalye <141475915+Rahul9146@users.noreply.github.com>
2024-06-17 10:46:47 +0530

    Add files via upload
    Comments implementation

--- Example1.java
+++ Example1.java
@@ -1,17 +1,23 @@
-package com.javabasic;
-
-public class Example1 {
-
+package com.java;
+          
+    public class Example1 {
+	     //This is Single line comment//
+          
 	public static void main(String[] args) {
-		//Typecasting Example
-		    double dNum = 100.04;
-	        int num = (int) dNum; // double is explicitly cast to int
-
-	        System.out.println("Double value: " + dNum);
-	        System.out.println("Integer value (after narrowing): " + num);
-	    }
+		/*
+        This is a multi-line comment.
+        This is a multi-line comment.
+        This is a multi-line comment.
+        */  
+			
+       System.out.println("Hello Guys,");
+       /**
+        * Adds two numbers together.
+         @html tags we used in comments.
+         @H2 tag is used to highlight text.
+        */         
+       
+       System.out.println("Today We learn Java comments");
 	}
 
-	
-
-
+}
